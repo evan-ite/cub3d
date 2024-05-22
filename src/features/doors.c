3ecd8ea8f6247617cfb823d18427b073d788582c
Jan Strozyk <jstrozyk@students.42berlin.de>
2024-05-22 14:16:49 +0200

    add open/close doors functionality

--- src/features/doors.c
+++ src/features/doors.c
@@ -6,7 +6,7 @@
 /*   By: jstrozyk <jstrozyk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 10:37:34 by jstrozyk          #+#    #+#             */
-/*   Updated: 2024/05/22 14:04:28 by jstrozyk         ###   ########.fr       */
+/*   Updated: 2024/05/22 14:15:55 by jstrozyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ void	open_doors(t_game *g)
 	char	*state;
 
 	state = &(g->map->m[g->player->object.y][g->player->object.x]);
-
 	if (*state == 'd')
 		*state = 'D';
-	if (*state =='D')
+	else if (*state =='D')
 		*state = 'd';
 }
